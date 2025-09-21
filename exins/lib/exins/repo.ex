801d6773e09538ccc4227d27ceea23be2ed4b191
Sarin Rajendran <sarin.rajendran@gmail.com>
2025-09-21 02:46:56 +0000

    Add `min_pg_version` to **repo**

--- exins/lib/exins/repo.ex
+++ exins/lib/exins/repo.ex
@@ -5,4 +5,8 @@ defmodule Exins.Repo do
   def installed_extensions do
     ["ash-functions", "uuid-ossp", "citext"]
   end
+
+  def min_pg_version do
+    %Version{major: 16, minor: 0, patch: 0}
+  end
 end
