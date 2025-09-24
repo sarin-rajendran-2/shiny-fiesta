574dec0ade2d84822c93051b40f0778a3f542904
Sarin Rajendran <sarin.rajendran@gmail.com>
2025-09-24 08:03:50 +0000

    Add Common **Phone** embedded resource

---
+++ exins/lib/exins/common/phone.ex
@@ -0,0 +1,25 @@
+defmodule Exins.Common.Phone do
+  @moduledoc """
+  The Phone resource.
+  """
+
+  use Ash.Resource,
+  data_layer: :embedded
+
+  actions do
+    defaults [:destroy, :read, create: :*, update: :*]
+  end
+
+  attributes do
+    uuid_v7_primary_key :id
+    attribute :type, :string, allow_nil?: true, public?: true
+    attribute :numbers, :map, allow_nil?: false, public?: true
+  end
+  calculations do
+    calculate :full_phone_number, :string, expr("+" <> numbers["country_code"] <> numbers["phone_number"])
+  end
+
+  preparations do
+    prepare build(load: [:full_phone_number])
+  end
+end
