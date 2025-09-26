fdf1613f23762dbba37a500ea3b02a92a565a256
Sarin Rajendran <sarin.rajendran@gmail.com>
2025-09-26 21:21:23 +0000

    Add **Exins.Common.Address** embedded resource to Contact

--- exins/lib/exins/common/contact.ex
+++ exins/lib/exins/common/contact.ex
@@ -20,6 +20,8 @@ defmodule Exins.Common.Contact do
       constraints [one_of: [:individual, :organization]]
     end
 
+    attribute :addresses, {:array, Exins.Common.Address}, public?: true, allow_nil?: true
+
     attribute :emails, {:array, Exins.Common.Email}, public?: true, allow_nil?: true
 
     attribute :names, {:array, Exins.Common.Name}, public?: true, allow_nil?: false
