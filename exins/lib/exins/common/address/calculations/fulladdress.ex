483f5f72a3451ae235e0968ca7059d2c339c2044
Sarin Rajendran <sarin.rajendran@gmail.com>
2025-09-27 20:17:30 +0000

    Add **FullAddress** calculation

---
+++ exins/lib/exins/common/address/calculations/fulladdress.ex
@@ -0,0 +1,33 @@
+defmodule Exins.Common.FullAddressCalculation do
+  use Ash.Resource.Calculation
+
+  @impl true
+  # A callback to tell Ash what keys must be loaded/selected when running this calculation
+  def load(_query, _opts, _context) do [:address_parts] end
+
+  def format_address(address, separator) do
+    case address.address_parts do
+      nil -> nil
+      parts when is_map(parts) ->
+        [
+          case parts["street_address"] do
+            street_addresses when is_list(street_addresses) -> Enum.join(street_addresses, separator)
+            street_address -> street_address
+          end,
+          parts["city"],
+          parts["state"],
+          parts["post_code"],
+          parts["postal_code"],
+          parts["country"]
+        ]
+        |> Enum.filter(& &1) # Remove nil or empty parts
+        |> Enum.join(separator)
+      _ -> "Unexpected"
+    end
+  end
+
+  @impl true
+  def calculate(addresses, _opts, %{arguments: %{separator: separator}}) do
+      Enum.map(addresses, &format_address(&1, separator))
+  end
+end
