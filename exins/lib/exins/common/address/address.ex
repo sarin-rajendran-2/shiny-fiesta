e7185e6f3b258a28f9a006c0225aac3d1729ec08
Sarin Rajendran <sarin.rajendran@gmail.com>
2025-09-27 20:17:30 +0000

    Add **FullAddress** calculation

--- exins/lib/exins/common/address/address.ex
+++ exins/lib/exins/common/address/address.ex
@@ -1,39 +1,5 @@
-defmodule Exins.Common.Calculations.FullAddress do
-  use Ash.Resource.Calculation
-
-  @impl true
-  # A callback to tell Ash what keys must be loaded/selected when running this calculation
-  def load(_query, _opts, _context) do [:address_parts] end
-
-  def format_address(address, separator) do
-    case address.address_parts do
-      nil -> nil
-      parts when is_map(parts) ->
-        [
-          case parts["street_address"] do
-            street_addresses when is_list(street_addresses) -> Enum.join(street_addresses, separator)
-            street_address -> street_address
-          end,
-          parts["city"],
-          parts["state"],
-          parts["post_code"],
-          parts["postal_code"],
-          parts["country"]
-        ]
-        |> Enum.filter(& &1) # Remove nil or empty parts
-        |> Enum.join(separator)
-      _ -> "Unexpected"
-    end
-  end
-
-  @impl true
-  def calculate(addresses, _opts, %{arguments: %{separator: separator}}) do
-      Enum.map(addresses, &format_address(&1, separator))
-  end
-end
-
 defmodule Exins.Common.Address do
-  alias Exins.Common.Calculations.FullAddress
+  alias Exins.Common.FullAddressCalculation
   @moduledoc """
   The Address resource.
   """
@@ -53,7 +19,7 @@ defmodule Exins.Common.Address do
   end
 
   calculations do
-    calculate :full_address, :string, {FullAddress, []} do
+    calculate :full_address, :string, {FullAddressCalculation, []} do
       argument :separator, :string, default: ", "
     end
   end
